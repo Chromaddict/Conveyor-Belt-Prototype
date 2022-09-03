803751a67096587353e77e1ffb7375e64d4a7209
Chromaddict <43283050+Chromaddict@users.noreply.github.com>
2022-09-03 09:21:33 -0500

    WebGL build

---
+++ ConveyorBelt/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,137 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Belt::OnTriggerStay(UnityEngine.Collider)
+extern void Belt_OnTriggerStay_mBBEDDB7DFA8A3DF72C8CC518F9484FFB177E9FE8 (void);
+// 0x00000002 System.Void Belt::OnTriggerExit(UnityEngine.Collider)
+extern void Belt_OnTriggerExit_mD22B161D0ADDA5338FD9946410848BB23B50B6C9 (void);
+// 0x00000003 System.Void Belt::.ctor()
+extern void Belt__ctor_m81CE685D3FA76F0111869232CCE51153C8755066 (void);
+// 0x00000004 System.Void Counter::OnTriggerEnter(UnityEngine.Collider)
+extern void Counter_OnTriggerEnter_m3E9E13D676B0DD5B44FCD09A8BF5F7268C7A0C52 (void);
+// 0x00000005 System.Void Counter::.ctor()
+extern void Counter__ctor_m8268512C5E82F5E698855F4CC1B317750CF97558 (void);
+// 0x00000006 GameManager GameManager::get_Instance()
+extern void GameManager_get_Instance_m076FE4D98E785B5AEE0B4C360C7857F824E7FBD0 (void);
+// 0x00000007 System.Void GameManager::set_Instance(GameManager)
+extern void GameManager_set_Instance_mCFE1FA9223DC000B8D5535A9B6576264FA4405EF (void);
+// 0x00000008 System.Int32 GameManager::get_Count()
+extern void GameManager_get_Count_mF9B4E7A87DCFC930347D23063FD476B393A88396 (void);
+// 0x00000009 System.Void GameManager::Awake()
+extern void GameManager_Awake_m4B6E8E2AF58C95C9A2A0C4637A34AE0892CB637F (void);
+// 0x0000000A System.Void GameManager::AddToCount()
+extern void GameManager_AddToCount_mD0321ED2FA01FD48045E74F792ACED81E9CF7371 (void);
+// 0x0000000B System.Void GameManager::SetSpeed(System.Single)
+extern void GameManager_SetSpeed_m5FA5CBAC49698F3063E71BF1A4E14B0348D587AF (void);
+// 0x0000000C System.Void GameManager::.ctor()
+extern void GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368 (void);
+// 0x0000000D System.Void Rotate::Start()
+extern void Rotate_Start_mD322E77A3CF2BEF28C4DF71D3F529107F511B1FB (void);
+// 0x0000000E System.Void Rotate::Update()
+extern void Rotate_Update_m73D585515036D9B7AAD8336BFB8567283CE4C7E7 (void);
+// 0x0000000F System.Void Rotate::.ctor()
+extern void Rotate__ctor_m0EE5CC8EB699542BFC438DC3D547D39E442E9EE4 (void);
+// 0x00000010 System.Void Spawner::Start()
+extern void Spawner_Start_m8BB68F2141CF386C08091E93308438F56B08EE73 (void);
+// 0x00000011 System.Collections.IEnumerator Spawner::SpawnCube()
+extern void Spawner_SpawnCube_m7F64A2FC58BDA536F6B252F83210831E2A029556 (void);
+// 0x00000012 System.Void Spawner::.ctor()
+extern void Spawner__ctor_mBF592E8E9B5682687D8C28E73A64BF29B6BF2088 (void);
+// 0x00000013 System.Void Spawner/<SpawnCube>d__5::.ctor(System.Int32)
+extern void U3CSpawnCubeU3Ed__5__ctor_m82B8EDA73D0B180CBF329D4E4E19562B4DB58A1E (void);
+// 0x00000014 System.Void Spawner/<SpawnCube>d__5::System.IDisposable.Dispose()
+extern void U3CSpawnCubeU3Ed__5_System_IDisposable_Dispose_m03456D0B71824CB53A8688D0E42F784E7E2F0504 (void);
+// 0x00000015 System.Boolean Spawner/<SpawnCube>d__5::MoveNext()
+extern void U3CSpawnCubeU3Ed__5_MoveNext_mD8FFA392D6C9DA85C65B534C7E4F0FA4C719943C (void);
+// 0x00000016 System.Object Spawner/<SpawnCube>d__5::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CSpawnCubeU3Ed__5_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m695FBE8D9038D12EBCB7F076225F52ED32ADE681 (void);
+// 0x00000017 System.Void Spawner/<SpawnCube>d__5::System.Collections.IEnumerator.Reset()
+extern void U3CSpawnCubeU3Ed__5_System_Collections_IEnumerator_Reset_m84FD970B5FC65C841E1797516D27BD0F356AB1F3 (void);
+// 0x00000018 System.Object Spawner/<SpawnCube>d__5::System.Collections.IEnumerator.get_Current()
+extern void U3CSpawnCubeU3Ed__5_System_Collections_IEnumerator_get_Current_m8B0A003D66595C84F8063053E90CFEA30C26F6DF (void);
+static Il2CppMethodPointer s_methodPointers[24] = 
+{
+	Belt_OnTriggerStay_mBBEDDB7DFA8A3DF72C8CC518F9484FFB177E9FE8,
+	Belt_OnTriggerExit_mD22B161D0ADDA5338FD9946410848BB23B50B6C9,
+	Belt__ctor_m81CE685D3FA76F0111869232CCE51153C8755066,
+	Counter_OnTriggerEnter_m3E9E13D676B0DD5B44FCD09A8BF5F7268C7A0C52,
+	Counter__ctor_m8268512C5E82F5E698855F4CC1B317750CF97558,
+	GameManager_get_Instance_m076FE4D98E785B5AEE0B4C360C7857F824E7FBD0,
+	GameManager_set_Instance_mCFE1FA9223DC000B8D5535A9B6576264FA4405EF,
+	GameManager_get_Count_mF9B4E7A87DCFC930347D23063FD476B393A88396,
+	GameManager_Awake_m4B6E8E2AF58C95C9A2A0C4637A34AE0892CB637F,
+	GameManager_AddToCount_mD0321ED2FA01FD48045E74F792ACED81E9CF7371,
+	GameManager_SetSpeed_m5FA5CBAC49698F3063E71BF1A4E14B0348D587AF,
+	GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368,
+	Rotate_Start_mD322E77A3CF2BEF28C4DF71D3F529107F511B1FB,
+	Rotate_Update_m73D585515036D9B7AAD8336BFB8567283CE4C7E7,
+	Rotate__ctor_m0EE5CC8EB699542BFC438DC3D547D39E442E9EE4,
+	Spawner_Start_m8BB68F2141CF386C08091E93308438F56B08EE73,
+	Spawner_SpawnCube_m7F64A2FC58BDA536F6B252F83210831E2A029556,
+	Spawner__ctor_mBF592E8E9B5682687D8C28E73A64BF29B6BF2088,
+	U3CSpawnCubeU3Ed__5__ctor_m82B8EDA73D0B180CBF329D4E4E19562B4DB58A1E,
+	U3CSpawnCubeU3Ed__5_System_IDisposable_Dispose_m03456D0B71824CB53A8688D0E42F784E7E2F0504,
+	U3CSpawnCubeU3Ed__5_MoveNext_mD8FFA392D6C9DA85C65B534C7E4F0FA4C719943C,
+	U3CSpawnCubeU3Ed__5_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m695FBE8D9038D12EBCB7F076225F52ED32ADE681,
+	U3CSpawnCubeU3Ed__5_System_Collections_IEnumerator_Reset_m84FD970B5FC65C841E1797516D27BD0F356AB1F3,
+	U3CSpawnCubeU3Ed__5_System_Collections_IEnumerator_get_Current_m8B0A003D66595C84F8063053E90CFEA30C26F6DF,
+};
+static const int32_t s_InvokerIndices[24] = 
+{
+	2785,
+	2785,
+	3419,
+	2785,
+	3419,
+	5114,
+	5041,
+	3307,
+	3419,
+	3419,
+	2816,
+	3419,
+	3419,
+	3419,
+	3419,
+	3419,
+	3327,
+	3419,
+	2767,
+	3419,
+	3267,
+	3327,
+	3419,
+	3327,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	24,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
